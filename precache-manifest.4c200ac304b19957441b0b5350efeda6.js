6008c40677617687c7ab0beee3a029e708a4ce4a
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-07-04 16:56:07 -0400

    Updates

---
+++ precache-manifest.4c200ac304b19957441b0b5350efeda6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794b931747bafe6651473b00b35ecdb0",
+    "url": "/Weather-React-App/index.html"
+  },
+  {
+    "revision": "0cba2484ecf93d2b7b78",
+    "url": "/Weather-React-App/static/css/main.a348e2e9.chunk.css"
+  },
+  {
+    "revision": "20f682600f3427846bf2",
+    "url": "/Weather-React-App/static/js/2.b4c69cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-React-App/static/js/2.b4c69cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cba2484ecf93d2b7b78",
+    "url": "/Weather-React-App/static/js/main.0c99e354.chunk.js"
+  },
+  {
+    "revision": "25e20c0ec8f8fbcbd8e5",
+    "url": "/Weather-React-App/static/js/runtime-main.c54c8523.js"
+  },
+  {
+    "revision": "6f1a6a820fa47cd1b7b1a25b753af7f7",
+    "url": "/Weather-React-App/static/media/cloud-rain.6f1a6a82.jpg"
+  },
+  {
+    "revision": "bbe2532ed038fc128abbde7e434f8b2f",
+    "url": "/Weather-React-App/static/media/nice-weather.bbe2532e.jpg"
+  }
+]);
